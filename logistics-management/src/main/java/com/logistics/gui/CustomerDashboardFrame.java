56f88b6220bd40fbdb61ba33efaea35725486fd1
ALAN JOY <alanjoy812006@gmail.com>
2025-10-03 18:20:34 +0530

    Update CustomerDashboardFrame.java

--- logistics-management/src/main/java/com/logistics/gui/CustomerDashboardFrame.java
+++ logistics-management/src/main/java/com/logistics/gui/CustomerDashboardFrame.java
@@ -1,1 +1,21 @@
+package com.logistics.gui;
 
+import com.logistics.models.User;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomerDashboardFrame extends JFrame {
+
+    public CustomerDashboardFrame(User user) {
+        setTitle("Customer Dashboard");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Welcome message
+        JLabel welcomeLabel = new JLabel("Welcome, " + user.getName() + "!", SwingConstants.CENTER);
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        add(welcomeLabel, BorderLayout.CENTER);
+    }
+}
