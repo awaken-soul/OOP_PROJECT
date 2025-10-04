487bc2142813a9357b8921c0a1f906ab9d12c2ac
ALAN JOY <alanjoy812006@gmail.com>
2025-10-04 09:50:31 +0530

    Update Dao.java

--- logistics-management/src/main/java/com/logistics/database/Dao.java
+++ logistics-management/src/main/java/com/logistics/database/Dao.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 public interface Dao<T> {
     Optional<T> findById(int id);
     List<T> findAll();
-    boolean save(T t);
+    Optional<Integer> save(T t); 
     boolean update(T t);
     boolean delete(T t);
 }
